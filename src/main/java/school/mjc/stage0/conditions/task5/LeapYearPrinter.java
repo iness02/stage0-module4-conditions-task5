0be25d1608f62f2d138ff7eb63ac90fbbfda142f
inesa_hakobyan <inesahakobyan18@gmail.com>
2022-09-08 19:34:08 +0400

    done

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+       int isLeapYear = ((year%4==0)&&((year%400==0)||(year%100!=0)))?1:0;
+        switch (isLeapYear){
+            case 1:
+                System.out.println("leap");
+                break;
+            default:
+                System.out.println("not leap");
+
+        }
     }
 }
